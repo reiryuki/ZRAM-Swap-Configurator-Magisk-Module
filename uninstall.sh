d5384a967e2d3dd9a46492498bcef7b454343282
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-01-18 18:27:35 +0700

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 mount -o rw,remount /data
 [ ! "$MODPATH" ] && MODPATH=${0%/*}
 [ ! "$MODID" ] && MODID=`basename "$MODPATH"`
+UID=`id -u`
 
 # log
-exec 2>/data/media/0/$MODID\_uninstall.log
+exec 2>/data/media/"$UID"/$MODID\_uninstall.log
 set -x
 
 # run
